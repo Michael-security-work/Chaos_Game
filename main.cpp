7bd446e8f994b0a66eb6ca3e25993e38ffe0f046
Michael-security-work <mihail006.mn@gmail.com>
2025-11-21 22:31:23 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <sstream>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
+
+int main()
+{
+	cout << "sup" << endl;
+	VideoMode vm(1280, 800);
+	
+	return 0; 
+}
